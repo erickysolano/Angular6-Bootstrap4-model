19f6233043ad3aeb92d191d930e98b504aad3b1c
Sola <erickysolano@gmail.com>
2018-06-30 23:08:36 -0300

    05 - Criação dos components Home/Link/Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LinkComponent } from './link/link.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LinkComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
